053b7bc13e4fb74e91f3ab904cb47baa588105c0
ak752 <ak752@DESKTOP-VOSGIAN>
2023-05-25 03:30:36 +0200

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string[] array = arr.Text.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                int minNum = Convert.ToInt32(array.ToArray().Min());
+                if (res.Text is null)
+                    res.Text = minNum.ToString();
+                else
+                {
+                    res.Clear();
+                    res.Text = minNum.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
